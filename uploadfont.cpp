9684e4fbdd467ddbd9a132b29088ce9d659f4800
Don Mills <don.mills@gmail.com>
2021-01-26 18:04:21 -0500

    Update uploadfont.cpp
    fixed an old static filesystem path

--- uploadfont.cpp
+++ uploadfont.cpp
@@ -1,6 +1,6 @@
 #include <EEPROM.h>
 #include <Binary.h>
-#include "C:\Documents and Settings\D\My Documents\Arduino\scrollkickass\letters.h"
+#include ".\letters.h"
 
 const uint8_t * alphabet[] =  {_A,_B,_C,_D,_E,_F,_G,_H,_I,_J,_K,_L,_M,_N,_O,_P,_Q,_R,_S,_T,_U,_V,_w,_X,_Y,_Z,__,_exclaim,_heart,_Alien1,_Alien2,_Alien1b,_Alien3,_Alien3b,check4a, check4b, fillbox1,fillbox2,fillbox3,allfull,check2a,check2b,horzin1,horzin2,horzin3,fana1,fana2,fana3,fanb1,fanb2,fanb3};
 
